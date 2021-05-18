09aa62fefd9f9dd64ef8d4972dcee43d41e71c57
Pinxuan Huang <pinxhuang@gmail.com>
2021-05-18 12:20:20 -0400

    Implement pagination for gallery

--- assets/js/gallery_scripts.js
+++ assets/js/gallery_scripts.js
@@ -1,21 +1,29 @@
 ---
 ---
 
-// checkbox filter source: https://codepen.io/piotrek/pen/mXpRmQ?editors=1010
+// Checkbox filter variables
 var allCheckboxes = [];
 var checked = {};
+var resIds = []; // a list of ids that matches the selected categories
+
+// Pagination variables
+var limit = 12; // number of games per page
+var currPage = 1; // current page number
+var maxPage = 1; // maximum number of pages
 
 window.onload = function () {
     // Get initial checkboxes
-    allCheckboxes = document.querySelectorAll('.check-box');
+    this.allCheckboxes = document.querySelectorAll('.check-box');
     getChecked('selectedGenres');
-    getChecked('selectedYears');
     getChecked('selectedCourses');
 
     // Add event listener to checkboxes
-    Array.prototype.forEach.call(allCheckboxes, function (el) {
+    Array.prototype.forEach.call(this.allCheckboxes, function (el) {
         el.addEventListener('change', toggleCheckbox);
     });
+
+    // Populate resIds with all games
+    filterResults();
 
     // Collapsible containers
     // Source: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_collapsible_symbol
@@ -34,50 +42,131 @@ window.onload = function () {
     };
 };
 
+// ======================= Filter functions =======================
+// Checkbox filter source: https://codepen.io/piotrek/pen/mXpRmQ?editors=1010
 function toggleCheckbox(e) {
     getChecked(e.target.name);
     filterResults();
 };
 
 function getChecked(name) {
-    checked[name] = Array.from(document.querySelectorAll('input[name=' + name + ']:checked')).map(function (el) {
-        return el.value;
+    this.checked[name] = Array.from(
+        document.querySelectorAll(
+            'input[name=' + name + ']:checked')
+        ).map(function (el) {
+            return el.value;
     });
 };
 
+// Returns true if any category is selected
+function isCheckedEmpty() {
+    for (const [key, value] of Object.entries(this.checked)) {
+        if (value.length > 0) return false;
+    };
+    return true;
+}
+
 function filterResults() {
-    var hasResult = false;
+    this.resIds = [];
+    this.currPage = 1;
+    
     var id = 0;
     {% for game in site.data.showcase %}
     {% capture gid %}{{ game.id }}{% endcapture %}
     {% assign details = site.data.gallery["2020"][gid] %}
+    id++;
 
-    var details = {{ details | jsonify}};
-    var gameContainer = document.getElementById(++id);
-
-    var matchesGenre = checked.selectedGenres.length == 0 ||
-        checked.selectedGenres.some(x => details.genres.includes(x));
-    var matchesCourse = checked.selectedCourses.length == 0 ||
-        checked.selectedCourses.includes(details.course.toString());
-
-    // Hide classes that do not match selected options
-    if (matchesGenre && matchesCourse) {
-        removeClass(gameContainer, "hidden");
-        hasResult = true;
-    } else {
-        addClass(gameContainer, "hidden");
+    if (isCheckedEmpty()) { 
+        // simply add the id if no category is checked
+        this.resIds.push(id);
+    } else { 
+        // otherwise filter through the games that match the selected categories
+        var details = {{ details | jsonify}};
+        var matchesGenre = this.checked.selectedGenres.length == 0 ||
+            this.checked.selectedGenres.some(x => details.genres.includes(x));
+        var matchesCourse = this.checked.selectedCourses.length == 0 ||
+            this.checked.selectedCourses.includes(details.course.toString());
+        if (matchesGenre && matchesCourse) this.resIds.push(id);
     };
     {% endfor %}
 
+    renderResults();
+    setMaxPage();
+
+    // display a message if no games match the selected categories
     var noResults = document.getElementById("no-results");
-    if (!hasResult) {
+    if (this.resIds.length === 0) {
         removeClass(noResults, "hidden");
     } else {
         addClass(noResults, "hidden");
     };
 };
 
-// Code source: https://stackoverflow.com/questions/26736587/how-to-add-and-remove-classes-in-javascript-without-jquery
+function renderResults() {
+    // Calculate which portion of games should be displayed on the current page
+    var start = (this.limit) * (this.currPage - 1);
+    var end = ((this.limit) * this.currPage);
+    end = end > this.resIds.length ? this.resIds.length : end;
+    var resIdPortion = this.resIds.slice(start, end);
+    // console.log(`start, end: ${start}, ${end}`);
+    // console.log('resIdPortion');
+    // console.log(resIdPortion);
+    var id = 0;
+    {% for game in site.data.showcase %}
+    id++;
+    var gameContainer = document.getElementById(id);
+    // Show games that belong to the current page
+    if (resIdPortion.includes(id)) {
+        removeClass(gameContainer, "hidden");
+    } else {
+        addClass(gameContainer, "hidden");
+    };
+    {% endfor %}
+}
+
+// ======================= Functions for pagination =======================
+function setMaxPage() {
+    this.maxPage = Math.ceil(this.resIds.length / this.limit);
+    renderPageButtons();
+}
+
+function goNextPage() {
+    if (this.currPage + 1 <= this.maxPage) {
+        this.currPage++;
+    };
+    renderPageButtons();
+    renderResults();
+    // console.log(`currPage: ${this.currPage}`);
+    // console.log(`maxPage: ${this.maxPage}`);
+    // console.log(this.resIds);
+};
+
+function goPrevPage() {
+    if (this.currPage - 1 > 0) {
+        this.currPage--;
+    };
+    renderPageButtons();
+    renderResults();
+};
+
+// Show relevant page buttons
+function renderPageButtons() {
+    var nextPage = document.getElementById("next-page");
+    if (this.currPage < this.maxPage) {
+        removeClass(nextPage, "hidden"); // show the next page button
+    } else {
+        addClass(nextPage, "hidden"); // hide the next page button
+    };
+    var prevPage = document.getElementById("prev-page");
+    if (this.currPage > 1) {
+        removeClass(prevPage, "hidden"); // show the previous page button
+    } else {
+        addClass(prevPage, "hidden"); // hide the previous page button
+    };
+};
+
+// ========== Functions to remove or add class names to elements ========== 
+// Source: https://stackoverflow.com/questions/26736587/how-to-add-and-remove-classes-in-javascript-without-jquery
 function hasClass(el, className) {
     if (el.classList)
         return el.classList.contains(className);
