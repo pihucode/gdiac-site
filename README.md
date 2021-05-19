aa1742b042db8b41e48aa5a25c2b7b2d8811a06c
rmz38 <rmz38@cornell.edu>
2021-05-19 02:02:43 -0400

    finished featured banner and making pages responsive on mobile

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # gdiac-site
  
+**Replace the content of the contact page and the urls of the footer**
+
+# To change Featured Games:
+Update the _data/featured_games.yml file, currently picked games that won awards in 2020 as featured
--- assets/js/gallery_scripts.js
+++ assets/js/gallery_scripts.js
@@ -10,7 +10,9 @@ var resIds = []; // a list of ids that matches the selected categories
 var limit = 12; // number of games per page
 var currPage = 1; // current page number
 var maxPage = 1; // maximum number of pages
-var currBanner = 1;
+var prevBanner = 1;
+var currBanner = 2;
+var nextBanner = 3;
 
 window.onload = function () {
     // Get initial checkboxes
@@ -197,7 +199,37 @@ function removeClass(el, className) {
     }
 };
 
-// function printer(item) {
-//     console.log(item);
-// }
-// function increment(index, )
+function prevFeatured(banners, baseurl) {
+    nextBanner = currBanner;
+    currBanner = prevBanner;
+    prevBanner -= 1;
+    var banners = banners.trim().split(" ");
+    console.log(banners);
+    if( prevBanner < 0){
+        prevBanner = banners.length - 1;
+    }
+    document.getElementById("featured-left-img").src = baseurl + "/gallery/" + banners[prevBanner] + "/banner.png"
+    document.getElementById("featured-center-img").src = baseurl + "/gallery/" + banners[currBanner] + "/banner.png"
+    document.getElementById("featured-right-img").src = baseurl + "/gallery/" + banners[nextBanner] + "/banner.png"
+    document.getElementById("featured-left").href = baseurl + "/gallery/" + banners[prevBanner];
+    document.getElementById("featured-center").href = baseurl + "/gallery/" + banners[currBanner];
+    document.getElementById("featured-right").href = baseurl + "/gallery/" + banners[nextBanner];
+}
+
+function nextFeatured(banners, baseurl) {
+    prevBanner = currBanner;
+    currBanner = nextBanner;
+    nextBanner += 1;
+    var banners = banners.trim().split(" ");
+    console.log(banners);
+    if( nextBanner >= banners.length){
+        nextBanner = 0;
+    }
+    document.getElementById("featured-left-img").src = baseurl + "/gallery/" + banners[prevBanner] + "/banner.png";
+    document.getElementById("featured-center-img").src = baseurl + "/gallery/" + banners[currBanner] + "/banner.png";
+    document.getElementById("featured-right-img").src = baseurl + "/gallery/" + banners[nextBanner] + "/banner.png";
+    document.getElementById("featured-left").href = baseurl + "/gallery/" + banners[prevBanner];
+    document.getElementById("featured-center").href = baseurl + "/gallery/" + banners[currBanner];
+    document.getElementById("featured-right").href = baseurl + "/gallery/" + banners[nextBanner];
+
+}
